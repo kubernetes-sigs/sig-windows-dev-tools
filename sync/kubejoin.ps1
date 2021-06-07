b49de1ee35cfe083ef9e1d26b71f69915feed5b3
jay vyas <jayunit100@jays-MacBook-Pro.local>
2021-06-07 17:12:52 -0400

    remove old ps1

--- sync/kubejoin.ps1
+++
@@ -1,3 +0,0 @@
-:path += ";C:\Program Files\containerd"
-[Environment]::SetEnvironmentVariable("Path", :Path, [System.EnvironmentVariableTarget]::Machine)
-kubeadm join 10.0.2.15:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 7fjfe4.g88j3cohkjuyaxeo     --discovery-token-ca-cert-hash sha256:20ce09aace70da710531a7ff140e200444a70b08ad00d2dab77b260226ad24a8 
