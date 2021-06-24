e6002735e6369f9623d408a3a028aebd3e40ebb8
jay vyas <jvyas@vmware.com>
2021-06-24 12:32:35 -0400

    fixed

--- fetch.sh
+++ fetch.sh
@@ -17,27 +17,10 @@ limitations under the License.
 set -e
 variables_file="sync/shared/variables.yaml"
 
-# kubernetes version can be passed as param, otherwise it will be read from variables_file, otherwise it uses the default "1.21.0"
-kubernetes_version=$1
-if [ -z $kubernetes_version ]; then
-  if [ -f $variables_file ]; then
-    kubernetes_version=$(awk '/kubernetes_version_build/ {print $2}' $variables_file | sed -e 's/^"//' -e 's/"$//'); #read param ffrom file
-    echo "using kubernetes version $kubernetes_version (read from $variables_file)"
-  else 
-    kubernetes_version="1.21.0"
-    echo "using kubernetes version $kubernetes_version (using default)"
-  fi
-else
-  echo "using kubernetes version $kubernetes_version (passed as parameter)"
-fi
-
-if [[ -d "kubernetes" ]] ; then
-	echo "kubernetes/ exists, not cloning..."
-else
-  echo "clone kubernetes..."
-  git clone https://github.com/kubernetes/kubernetes.git --branch v$kubernetes_version
-fi
-
+git clone https://github.com/kubernetes/kubernetes.git
+pushd kubernetes
+git checkout cb303e613a121a29364f75cc67d3d580833a7479
+popd
 # BELOW THIS LINE ADD YOUR CUSTOM BUILD LOGIC #########
 # FOR EXAMPLE
 # pushd kubernetes
