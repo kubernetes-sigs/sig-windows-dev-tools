6e409964530b43d080291325082fa389c921ddd0
jay vyas <jvyas@vmware.com>
2021-06-29 21:43:06 -0400

    remove k8s version windows

--- sync/shared/kubejoin.ps1
+++ sync/shared/kubejoin.ps1
@@ -1,4 +1,3 @@
 $env:path += ";C:\Program Files\containerd"
 [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 19f1py.onamsn7i80hphxmh --discovery-token-ca-cert-hash sha256:be279c00a53961ee091d10fbc2a33ba09f850b08fe06ba313ddb03302be26d59
-
+kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 9n3umk.o2iqrho7rjakrqps --discovery-token-ca-cert-hash sha256:045241bfcea2c884eceef830db4dcf9432c5f0aa8d00fbe2f54710f4bc284cdb 
