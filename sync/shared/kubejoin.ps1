bb11056d9719e576b0eddc4749dd0f849e0ac533
jay vyas <jvyas@vmware.com>
2021-07-01 09:31:00 -0400

    Antrea retry (#41)
    * Antrea retries if kube proxy not online yet, restart final print services

--- sync/shared/kubejoin.ps1
+++ sync/shared/kubejoin.ps1
@@ -1,3 +1,3 @@
 $env:path += ";C:\Program Files\containerd"
 [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 9n3umk.o2iqrho7rjakrqps --discovery-token-ca-cert-hash sha256:045241bfcea2c884eceef830db4dcf9432c5f0aa8d00fbe2f54710f4bc284cdb 
+kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 86039w.99bp7lykobg831qx --discovery-token-ca-cert-hash sha256:6f4cac90bb19a1af3c620eb4bbd015d00b2181653ab6f36a3bf5ebce0dc01e76 
