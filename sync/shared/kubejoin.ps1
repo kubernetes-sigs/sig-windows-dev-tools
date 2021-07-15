56f8f10013c792a7e0aaa8e055e669529cba0cfe
jay vyas <jvyas@vmware.com>
2021-07-15 09:11:25 -0400

    Calico fix : use the right service-cidr (#62)

--- sync/shared/kubejoin.ps1
+++ sync/shared/kubejoin.ps1
@@ -1,3 +1,3 @@
 $env:path += ";C:\Program Files\containerd"
 [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token ttcnkw.7vtvjbj0r2l7zxcw --discovery-token-ca-cert-hash sha256:d8856a1e5474d6fea562f3b8a99b5db806f4e1a5fc110a7841582b9fda632c49 
+kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 5y178d.8hyknflg5s1kcycg --discovery-token-ca-cert-hash sha256:a774745870de218114f0573a2355da9a6c0fa6d6c70f1ad600f9bc32674be064 
