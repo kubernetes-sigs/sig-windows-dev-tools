6c6d7e0983df79b1d4f0cc57dcea8e6ba396f460
jay vyas <jvyas@vmware.com>
2021-07-27 06:23:02 -0400

    changed to vxlan

--- sync/shared/kubejoin.ps1
+++ sync/shared/kubejoin.ps1
@@ -1,3 +1,3 @@
 $env:path += ";C:\Program Files\containerd"
 [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token 5y178d.8hyknflg5s1kcycg --discovery-token-ca-cert-hash sha256:a774745870de218114f0573a2355da9a6c0fa6d6c70f1ad600f9bc32674be064 
+kubeadm join 10.20.30.10:6443 --cri-socket "npipe:////./pipe/containerd-containerd" --token xw77ff.dhtsi2cldqqfwxct --discovery-token-ca-cert-hash sha256:b7ccdebdc818d1d527eb4072177b9991dbd7fca2a32647cf9020cb419a7cf0da 
