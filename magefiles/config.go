d7d3b31ef8f7392e6ceee2997ed7911c8d06f0ec
Mateusz Łoskot <mateusz@loskot.net>
2023-04-30 23:51:26 +0200

    Users expects their SWDT_SETTINGS_FILE to be used

--- magefiles/config.go
+++ magefiles/config.go
@@ -130,7 +130,9 @@ func findSettingsFile() (string, error) {
 	if settingsFile != "" {
 		_, err := os.Stat(settingsFile)
 		if os.IsNotExist(err) {
-			settingsFile = ""
+			// Users expects the settings file they specify to be used,
+			// without any unexpected fallback to default settings file (below).
+			return "", fmt.Errorf("User-defined SWDT_SETTINGS_FILE=%s not found", settingsFile)
 		}
 	}
 	// Alternatively, user may have created local variables file
